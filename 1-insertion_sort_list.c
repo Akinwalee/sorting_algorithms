a086de9821e19c09effc48fdf382a9829c477cfb
Akinwalee <obatulafuad@gmail.com>
2023-12-31 16:35:34 +0000

    Updated files

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -40,8 +40,8 @@ void insertion_sort_list(listint_t **list)
 			temp->next = current;
 			current->prev = temp;
 		}
-		print_list(*list);
 		current = next;
+		print_list(sorted);
 	}
 	*list = sorted;
 }
