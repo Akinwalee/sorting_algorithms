5b138cc59fcb404c4fac45c1e1ff50ff5282c330
Akinwalee <obatulafuad@gmail.com>
2023-12-31 10:29:35 +0000

    Updated files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array using bubble sort algorithm
+ *
+ * @array: The array to sort
+ * @size: The size of the array
+ *
+ * Return: Nothing. Just prints the array after each swap
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int temp, swaps = 0;
+	size_t i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < size; j++)
+		{
+			if (array[j - 1] > array[j])
+			{
+				temp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
+				print_array(array, size);
+				swaps++;
+			}
+		}
+		if (swaps < 1)
+			break;
+	}
+}
